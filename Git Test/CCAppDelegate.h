dbe8db69ef193ef8b0b6b4900266ce4bf574057b
Diego Taquiri Diaz <dtaquiri@MacBook-Pro-de-Diego-2.local>
2014-04-03 10:09:14 -0500

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Diego Taquiri Diaz on 3/04/14.
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Diego Taquiri Diaz on 3/04/14.
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
