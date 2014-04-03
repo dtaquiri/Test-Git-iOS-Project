4c44b73f3ae7d9794142e095f6d7bb15688439bf
Diego Taquiri Diaz <dtaquiri@MacBook-Pro-de-Diego-2.local>
2014-04-03 10:19:33 -0500

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Diego Taquiri Diaz on 3/04/14.
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Diego Taquiri Diaz on 3/04/14.
+//  Copyright (c) 2014 iLDT. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
